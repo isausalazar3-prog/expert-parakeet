9ce21ca84de87217039a5272e95eb9c7b9134553
isausalazar3-prog <isausalazar3@gmail.com>
2026-03-11 21:15:58 -0600

    Add files via upload

--- index.html/script.js
+++ index.html/script.js
@@ -1,22 +1,32 @@
 
 function mostrarNumeros(){
 
-let contenedor = document.getElementById("numeros");
-contenedor.innerHTML="";
+let contenedor = document.getElementById("numeros")
+contenedor.innerHTML=""
 
 for(let i=0;i<100;i++){
 
-let numero=document.createElement("div");
+let numero=document.createElement("div")
 
-numero.classList.add("numero");
+numero.classList.add("numero")
 
-numero.innerText=i.toString().padStart(2,"0");
+numero.innerText=i.toString().padStart(2,"0")
 
 numero.onclick=function(){
-alert("Elegiste el número "+numero.innerText+"\nAhora envía el formulario para apartarlo.");
-};
 
-contenedor.appendChild(numero);
+let num = numero.innerText
+
+let telefono="523345143806"
+
+let mensaje="Hola, quiero apartar el numero "+num+" de la rifa de Mexirifas"
+
+let url="https://wa.me/"+telefono+"?text="+encodeURIComponent(mensaje)
+
+window.open(url,"_blank")
+
+}
+
+contenedor.appendChild(numero)
 
 }
 
